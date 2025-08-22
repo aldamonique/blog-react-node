47651fe3859c072bb9f18b5b2f6d1dbc4474c021
Alda Monique <aldamonique2828@gmail.com>
2025-08-22 12:35:03 -0300

    Feat: Improve responsiveness.

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ const Footer =() =>(
     <footer className="bg-dark text-white text-center  mt-auto">
         <p>© Copyright 2025 | By Alda Goncalves</p>
     </footer>
+    
 );
 
 export default Footer;
